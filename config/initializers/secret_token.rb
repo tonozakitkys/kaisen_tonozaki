215511bdfac458bf0e661254495b57c7b3a18a33
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-10-05 11:04:45 +0900

    rails new kaisen_tonozaki

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KaisenTonozaki::Application.config.secret_token = '849d840762dcad2db4e4033e1cc7838234634a8857299324288139dbffe8b53780464e5baaba1f0c2c4c8b3ffc8c162e60b9b7e424f2a8ade3c09901cc26ccfa'
