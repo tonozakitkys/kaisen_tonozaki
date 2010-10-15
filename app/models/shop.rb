c9ff80e73ea069cfd96f3b6525b6099dd86c2ce8
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-10-15 15:48:41 +0900

    dd validation to shops

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,6 @@
 class Shop < ActiveRecord::Base
+  validates_presence_of :name, :description, :lines_summary
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,
+                      :messsage => "はGIF,JPG,PNG画像のURLでなければなりません"
+  validates_uniqueness_of :name
 end
