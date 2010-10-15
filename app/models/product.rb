929d74870d305256060faedeb9a86188b99c8ed7
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-10-15 16:15:18 +0900

    add "price must be a multiple of hundreds"

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,11 @@ class Product < ActiveRecord::Base
   validates_format_of :image_url, :with => %r{\.(gif|jpg|png)\Z}i,
                       :message => "はGIF,JPG,PNG画像のURLでなければなりません"
   validates_uniqueness_of :name
+
+  validate :price_must_be_multiple_of_hundreds
+
+  private
+  def price_must_be_multiple_of_hundreds
+    errors.add(:price, "must be multiple of hundreds") if price % 100 != 0
+  end
 end
