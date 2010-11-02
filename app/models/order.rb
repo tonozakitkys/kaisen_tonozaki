c31df280683c23d49fc626269bd85dd6160b7ccc
tonozakitkys@gmail.com <tono@zantare.(none)>
2010-11-02 11:52:17 +0900

    add nested form for line items to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,6 +5,7 @@ class Order < ActiveRecord::Base
     ["注文書", "po"]
   ]
   has_many :line_items
+  accepts_nested_attributes_for :line_items, :reject_if => lambda {|a| a.values.all?(&:blank?)}, :allow_destroy => true
 
   validates_presence_of :name, :address, :email, :pay_type
   validates_inclusion_of :pay_type, :in => PAYMENT_TYPES.map(&:last)
